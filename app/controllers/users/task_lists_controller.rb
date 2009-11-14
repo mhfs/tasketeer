72525b45b4c79f56e30dcb6bd9418d4442b21556
Marcelo Silveira <grande@gmail.com>
2009-11-14 14:32:42 -0200

    improved controller and replaced user_task_list like calls by inherited resources url helpers

--- app/controllers/users/task_lists_controller.rb
+++ app/controllers/users/task_lists_controller.rb
@@ -1,22 +1,10 @@
 class Users::TaskListsController < InheritedResources::Base
+  defaults :route_prefix => ''
   before_filter :authenticate_user!
   belongs_to :user
-  
-  def create
-    create! { |success, failure| success.html { redirect_to user_task_lists_path(@user) } }
-  end
-  
-  def update
-    update! { |success, failure| success.html { redirect_to user_task_lists_path(@user) } }
-  end
-  
-  def destroy
-    destroy! { user_task_lists_path(@user) }
-  end
   
   protected
     def begin_of_association_chain
       @current_user
     end
-  
 end
